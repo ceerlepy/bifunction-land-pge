7ff92eeaf23e1e84c6ff0829c2aea609a488ba04
Veysel <veyseltosun.vt@gmail.com>
2019-10-04 22:26:49 +0100

    First commit bifunction

---
+++ src/pages/index.js
@@ -0,0 +1,299 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Scroller from "../components/scroller"
+import PortfolioModal from "../components/portfolio/modal"
+import PortfolioCarousel from "../components/portfolio/carousel"
+
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    Scroller.handleAnchorScroll = Scroller.handleAnchorScroll.bind(this)
+    this.state = {
+      modalShow: false,
+      modalCurrent: 0,
+    }
+    this.handlePortfolioClick = this.handlePortfolioClick.bind(this)
+    this.setModal = this.setModal.bind(this)
+  }
+
+  handlePortfolioClick(index, e) {
+    e.preventDefault()
+    this.setModal(true, index)
+  }
+
+  setModal(isShown, current) {
+    this.setState({
+      modalShow: isShown,
+      modalCurrent: current,
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <section className="page-section bg-primary" id="about">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 text-center">
+                <h2 className="text-white mt-0">LET'S WORK TOGETHER</h2>
+                <hr className="divider light my-4" />
+                <p className="text-white-50 mb-4">
+                  We are experienced software professionals. Our team delivers
+                  great applications using deep technical knowledge and strong
+                  practices in the development process.
+                </p>
+                <a
+                  className="btn btn-light btn-xl js-scroll-trigger"
+                  href="#services"
+                  onClick={Scroller.handleAnchorScroll}
+                >
+                  LET'S BEGIN
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="page-section" id="services">
+          <div className="container">
+            <h2 className="text-center mt-0">At Your Service</h2>
+            <hr className="divider my-4" />
+            <div className="row">
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-gem text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">WEB DEVELOPMENT</h3>
+                  <p className="text-muted mb-0">
+                    We offer custom, high performance, user-friendly web-based
+                    applications for every industry with ReactJS/Redux
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">SOFTWARE</h3>
+                  <p className="text-muted mb-0">
+                    We offer reliable, secure and clean software solutions
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-globe text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">TEAM</h3>
+                  <p className="text-muted mb-0">
+                    Our dedicated team produce successful project
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-heart text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">CLIENT</h3>
+                  <p className="text-muted mb-0">
+                    We are really customer-obsessed
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="portfolio">
+          <div className="container-fluid p-0">
+            <div className="row no-gutters">
+              <div className="col-lg-4 col-sm-6">
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/1.jpg"
+                  onClick={this.handlePortfolioClick.bind(this, 0)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[0].node.childImageSharp.fluid
+                    }
+                  />
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Category
+                    </div>
+                    <div className="project-name">pfunctionPMTool</div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/2.jpg"
+                  onClick={this.handlePortfolioClick.bind(this, 1)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[1].node.childImageSharp.fluid
+                    }
+                  />
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Category
+                    </div>
+                    <div className="project-name">bayi-react</div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/3.jpg"
+                  onClick={this.handlePortfolioClick.bind(this, 2)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[2].node.childImageSharp.fluid
+                    }
+                  />
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Category
+                    </div>
+                    <div className="project-name">BiFunctionBPMTool</div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a
+                  className="portfolio-box"
+                  href="images/portfolio/fullsize/4.jpg"
+                  onClick={this.handlePortfolioClick.bind(this, 3)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[3].node.childImageSharp.fluid
+                    }
+                  />
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Category
+                    </div>
+                    <div className="project-name">personal-blog</div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/5.jpg"
+                  onClick={this.handlePortfolioClick.bind(this, 4)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[4].node.childImageSharp.fluid
+                    }
+                  />
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Category
+                    </div>
+                    <div className="project-name">ChatInTalk</div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/6.jpg"
+                  onClick={this.handlePortfolioClick.bind(this, 5)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[5].node.childImageSharp.fluid
+                    }
+                  />
+                  <div className="portfolio-box-caption p-3">
+                    <div className="project-category text-white-50">
+                      Category
+                    </div>
+                    <div className="project-name">social-app-functions</div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* <section className="page-section bg-dark text-white">
+          <div className="container text-center">
+            <h2 className="mb-4">Free Download at Start Bootstrap!</h2>
+            <a
+              className="btn btn-light btn-xl"
+              href="https://startbootstrap.com/themes/creative/"
+            >
+              Download Now!
+            </a>
+          </div>
+        </section> */}
+
+        <section className="page-section" id="contact">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 text-center">
+                <h2 className="mt-0">Let's Get In Touch!</h2>
+                <hr className="divider my-4" />
+                <p className="text-muted mb-5">
+                  Ready to start your next project with us? Give us a call or
+                  send us an email and we will get back to you as soon as
+                  possible!
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
+                <div>+44 (746) 023-9206</div>
+              </div>
+              <div className="col-lg-4 mr-auto text-center">
+                <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                <a className="d-block" href="mailto:info@bifunction.com">
+                  info@bifunction.com
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <PortfolioModal
+          show={this.state.modalShow}
+          onHide={() => this.setModal(false, 0)}
+        >
+          <PortfolioCarousel
+            images={this.props.data.images.edges}
+            current={this.state.modalCurrent}
+          />
+        </PortfolioModal>
+      </Layout>
+    )
+  }
+}
+
+export const imageData = graphql`
+  query {
+    images: allFile(
+      filter: { relativePath: { glob: "portfolio/fullsize/*.jpg" } }
+      sort: { fields: name }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
